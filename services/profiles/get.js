8f0edf2db52d454df0fcc6d88e9b907a15940cd3
Jonas Barsten <jonasbarsten@gmail.com>
2019-06-27 01:00:47 +0200

    Debugging

--- services/profiles/get.js
+++ services/profiles/get.js
@@ -8,9 +8,9 @@ export async function main(event, context) {
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
-      // userId: event.requestContext.identity.cognitoIdentityId,
+      userId: event.requestContext.identity.cognitoIdentityId,
       // profileId: event.pathParameters.id
-      userId: event.pathParameters.id
+      // userId: event.pathParameters.id
     }
   };
 
