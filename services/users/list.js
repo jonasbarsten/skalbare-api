f12ffcf563bfd2f160c80befdaef532d9c8c1dbe
Jonas Barsten <jonasbarsten@gmail.com>
2019-06-30 03:50:09 +0200

    Adding list users API

--- services/users/list.js
+++ services/users/list.js
@@ -3,19 +3,13 @@ import { success, failure } from "../../libs/response-lib";
 
 export async function main (event, context) {
   const params = {
-    UserPoolId: process.env.cognitoIdentiyPoolId,
-    AttributesToGet: []
+    UserPoolId: process.env.cognitoIdentiyPoolId
   };
-
-  console.log(params);
 
   const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
-  try {
-    const result = await cognitoidentityserviceprovider.listUsers(params);
-    console.log(result);
-    return success(result.Items);
-  } catch (e) {
-    return failure({ status: false });
-  }
+  cognitoidentityserviceprovider.listUsers(params, function(err, data) {
+    if (err) console.log(err);
+    else console.log(data);
+  });
 }
