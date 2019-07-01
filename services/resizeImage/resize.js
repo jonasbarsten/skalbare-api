14d578d844f75ee5e3b01315ecd40b2d4f5734ac
Jonas Barsten <jonasbarsten@gmail.com>
2019-07-01 02:19:31 +0200

    Adding image resize API and allowing users to list items in bucket

---
+++ services/resizeImage/resize.js
@@ -0,0 +1,30 @@
+import { success, failure } from "../../libs/response-lib";
+import { s3Handler } from "../../libs/s3-lib";
+const sharp = require("sharp");
+
+export async function main (event) {
+
+  const { level, user, size, image } = event.pathParameters;
+
+  try {
+    const sizeArray = size.split('x');
+    const width = parseInt(sizeArray[0]);
+    const height = parseInt(sizeArray[1]);
+    const Key = `${level}/${user}/${image}`;
+    const newKey = `${level}/${user}/${width}x${height}/${image}`;
+
+    // TODO: set dynamically
+    const Bucket = "skalbare-api-dev-attachmentsbucket-1tsv3sa7oezub";
+    const streamResize = sharp().resize(width, height).toFormat('png');
+    const readStream = s3Handler.readStream({ Bucket, Key });
+    const { writeStream, uploaded } = s3Handler.writeStream({ Bucket, Key: newKey });
+
+    readStream.pipe(streamResize).pipe(writeStream);
+
+    await uploaded;
+    return success(newKey);
+
+  } catch (e) {
+    return failure({ status: false });
+  }
+};
